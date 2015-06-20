5fd4da66beaf45f2a406d5794ee4a64bd01e833e
Lee Clagett <code@leeclagett.com>
2015-06-19 23:10:24 -0400

    OCD ordering

--- include/prima/ir/base.hpp
+++ include/prima/ir/base.hpp
@@ -17,6 +17,20 @@ namespace ir
         using right = Right;
     };
 
+
+    template <typename Fields, typename Field>
+    using get_field_t = meta::at_t<Fields, Field>;
+
+    template <typename Fields, typename Field> constexpr auto get_field_value()
+    {
+        return get_field_t<Fields, Field>::value;
+    }
+
+    template <typename> constexpr bool has_field(...)
+    {
+        return false;
+    }
+
     template <template <unsigned> class Field> struct is_field_func
     {
         template <typename> struct apply : meta::false_
@@ -33,19 +47,6 @@ namespace ir
 
     template <typename Test, template <unsigned> class... Fields>
     using is_any_field = meta::or_<is_field<Test, Fields>...>;
-
-    template <typename> constexpr bool has_field(...)
-    {
-        return false;
-    }
-
-    template <typename Fields, typename Field>
-    using get_field_t = meta::at_t<Fields, Field>;
-
-    template <typename Fields, typename Field> constexpr auto get_field_value()
-    {
-        return get_field_t<Fields, Field>::value;
-    }
 } // ir
 } // prima
 
