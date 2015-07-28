88b808c8214c187da144a243a0ce59bee261da8a
Lee Clagett <code@leeclagett.com>
2015-07-27 22:00:19 -0400

    Added a few more backend/mixed tests

--- test/backend/mixed.hpp
+++ test/backend/mixed.hpp
@@ -21,6 +21,15 @@ namespace backend
             BOOST_TEST_EQ(
                 "<-%->",
                 (prima::test::generate<PRIMA_FMT("<-%%->"), Backend>()));
+            BOOST_TEST_EQ(
+                "Value:       6000\n",
+                (prima::test::generate<PRIMA_FMT("Value: %10u\n"), Backend>(6000u)));
+            BOOST_TEST_EQ(
+                "Value:       6000\nValue:       7000\n",
+                (prima::test::generate<PRIMA_FMT("Value: %10u\nValue: %10u\n"), Backend>(6000u, 7000u)));
+            BOOST_TEST_EQ(
+                "Value:     0X1770\nValue:     0x1b58\n",
+                (prima::test::generate<PRIMA_FMT("Value: %#10X\nValue: %#10x\n"), Backend>(6000u, 7000u)));
         }
     } // test
 } // backend
